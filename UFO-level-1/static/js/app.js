74c0613ecf945a67a1e354ed30375be18554816f
Jamie Diaz <jamie.diaz@gmail.com>
2019-09-15 02:34:57 -0500

    OMG it works!

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,12 +7,35 @@ var tableData = data;
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
-
+    
     var inputElement = d3.select("#datetime");
 
     var inputValue = inputElement.property("value");
+    
+    
 
-    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    var filteredData = tableData.filter(d => d.datetime === inputValue);
+    // function filterDate(dataItem) {
+    //     return dataItem.inputValue;
+    // }
+    // var filteredData = tableData.filter(filterDate);
 
+    var tbody = d3.select("tbody").text(" ");
+    
+    
+    console.log(filteredData);
 
+    filteredData.forEach(function(ufoData) {
+        console.log(ufoData);
+        var row = tbody.append("tr");
+
+        Object.entries(ufoData).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+
+        
+    });
+    
 });
